9b796bac6b23e937abf34bef39c626a123b40428
umar <umar1671594@gmail.com>
2023-11-09 20:49:26 +0500

    Main Page

---
+++ app/src/main/java/com/hammadmansoor/ecommerce/FashionSale.java
@@ -0,0 +1,14 @@
+package com.hammadmansoor.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FashionSale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fashion_sale);
+    }
+}
