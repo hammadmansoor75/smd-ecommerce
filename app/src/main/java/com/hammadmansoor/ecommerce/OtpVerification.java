1912ffb8c846a6c500edf492566b4b850ce1ab2d
umar <umar1671594@gmail.com>
2023-12-01 22:48:59 +0500

    recycler

--- app/src/main/java/com/hammadmansoor/ecommerce/OtpVerification.java
+++ app/src/main/java/com/hammadmansoor/ecommerce/OtpVerification.java
@@ -1,14 +1,63 @@
 package com.hammadmansoor.ecommerce;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class OtpVerification extends AppCompatActivity {
 
+    private EditText verificationCodeEditText;
+    private TextView displayNumberTextView;
+
+    // Variable to store the verification ID sent by Firebase
+    private String verificationId;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_verification);
+
+        // Initialize views
+        verificationCodeEditText = findViewById(R.id.verificationCodeEditText);
+        displayNumberTextView = findViewById(R.id.Display);
+
+        // Set click listener for the "SEND" button
+        Button verifyButton = findViewById(R.id.verifyButton);
+        verifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSendButtonClicked(view);
+            }
+        });
+
+        // Retrieve the verification ID passed from the previous activity
+        verificationId = getIntent().getStringExtra("verificationId");
+
+
     }
+
+    public void onSendButtonClicked(View view) {
+        // Get the entered verification code
+        String verificationCode = "000000";
+        // Verify the code
+        if (!verificationCode.isEmpty()) {
+            // Pass the verification code to the signup class for final verification
+            // For example, you can use an Intent to pass the verification code back to the signup class
+            Intent intent = new Intent();
+            intent.putExtra("verificationCode", verificationCode);
+            setResult(RESULT_OK, intent);
+            finish();
+        } else {
+            Toast.makeText(this, "Please enter the verification code", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Other methods and code as needed
+
 }
--- app/src/main/java/com/hammadmansoor/ecommerce/SignupPhone.java
+++ app/src/main/java/com/hammadmansoor/ecommerce/SignupPhone.java
@@ -1,36 +1,106 @@
 package com.hammadmansoor.ecommerce;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class SignupPhone extends AppCompatActivity {
 
-    TextView navigateSignupEmail,navigateLogin;
+    TextView navigateSignupEmail, navigateLogin;
+    FirebaseAuth firebaseAuth;
+    private String verificationId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_phone);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         navigateLogin = findViewById(R.id.navigateLogin);
         navigateLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignupPhone.this,LoginPhone.class));
+                startActivity(new Intent(SignupPhone.this, LoginPhone.class));
             }
         });
 
-        navigateSignupEmail = findViewById(R.id.navigateSignupEmail);
+        navigateSignupEmail = findViewById(R.id.signupPhone);
         navigateSignupEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignupPhone.this,SignupEmail.class));
+                // Start the phone authentication process
+                startPhoneAuthentication();
             }
         });
+    }
 
+    private void startPhoneAuthentication() {
+        // You can add code here to validate the phone number if needed
+
+        // Start the Firebase Phone Authentication process
+        String phoneNumber = "+923165583118"; // Replace with the user's phone number
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,
+                60,
+                TimeUnit.SECONDS,
+                this,
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        // Automatically handle verification if the code is received without user input
+                        signInWithPhoneAuthCredential(phoneAuthCredential);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        Toast.makeText(SignupPhone.this, "Verification failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(s, forceResendingToken);
+                        // Save the verification ID and start the OtpVerification activity
+                        verificationId = s;
+                        startOtpVerificationActivity();
+                    }
+                }
+        );
+    }
+
+    private void startOtpVerificationActivity() {
+        Intent intent = new Intent(SignupPhone.this, OtpVerification.class);
+        intent.putExtra("verificationId", verificationId);
+        startActivity(intent);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(SignupPhone.this, new OnCompleteListener<com.google.firebase.auth.AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<com.google.firebase.auth.AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Verification completed successfully
+                            Toast.makeText(SignupPhone.this, "Verification completed successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Verification failed
+                            Toast.makeText(SignupPhone.this, "Verification failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
